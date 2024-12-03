cf83612a2f425f731a99d9246892dbe64c7f3b5f
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-03 14:00:48 +0100

    creazione rotte per i posts

---
+++ assets/app/index.js
@@ -0,0 +1,28 @@
+/**
+    - Usando l'array dei post con le relative immagini, creare un file di routing (routers/posts.js) che conterrà le rotte necessarie per l'entità post.
+    -All'interno creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete
+    - Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, secondo le convenzioni REST. 
+    Ad esempio:
+    Se viene chiamata /posts col verbo GET ci aspettiamo “Lista dei post”;
+    Se viene chiamato /posts/1 col verbo DELETE ci aspettiamo “Cancellazione del post 1”
+    e via dicendo… 
+    - Registrare il router dentro app.js con il prefisso /posts.
+    ### Bonus
+    - Provare a restituire la lista dei post dalla rotta index, in formato json
+    - Provare a restituire un singolo post dalla rotta show, sempre in formato json
+ */
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const HOST = `http://localhost:${PORT}`;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("testing");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server aperto sulla porta ${PORT}
+        ${HOST}`);
+});
---
+++ assets/routers/posts.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+// routes
+
+// index
+router.get("/", (req, res) => {
+    res.send("index operation");
+});
+// show
+router.get("/:id", (req, res) => {
+    res.send("show operation -> id selected:", req.params.id);
+});
+// store
+router.post("/", (res, req) => {
+    res.send("store operation");
+});
+// update
+router.put("/:id", (req, res) => {
+    res.send("update operation -> id selected:", req.params.id);
+});
+// modify
+router.patch("/:id", (req, res) => {
+    res.send("partial modify operation -> id selected:", req.params.id);
+});
+// destroy
+router.delete("/:id", (req, res) => {
+    res.send("destroy operation -> id selected:", req.params.id);
+});
